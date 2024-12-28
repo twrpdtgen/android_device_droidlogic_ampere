6ca7d4240c69b575523a29579bc3282c358cc584
XvSvM <vadiagadia@gmail.com>
2024-12-29 01:29:08 +0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_ampere-user
-add_lunch_combo omni_ampere-userdebug
-add_lunch_combo omni_ampere-eng
+add_lunch_combo twrp_MTS_TV-user
+add_lunch_combo twrp_MTS_TV-userdebug
+add_lunch_combo twrp_MTS_TV-eng
